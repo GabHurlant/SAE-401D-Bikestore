675c836b27a7dc8c5bf0c2a5d1bf483e290abb9b
GabHurlant <gabin.lasne@gmail.com>
2024-04-04 01:22:04 +0200

    Mise à jour des liens du logo dans les fichiers de pied de page

--- view/FooterCustomer.php
+++ view/FooterCustomer.php
@@ -21,10 +21,8 @@
 </head>
 <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
     <div class="col mb-3">
-        <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
-            <svg class="bi me-2" width="40" height="32">
-                <use xlink:href="#bootstrap"></use>
-            </svg>
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <img src="" alt="Bikestore Logo">
         </a>
         <p class="text-body-secondary">© 2024</p>
     </div>
